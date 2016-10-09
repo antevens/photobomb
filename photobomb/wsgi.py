905b9c055030f466d52b151e37301d7aa4211368
Antonia Stevens <a@antevens.com>
2016-10-08 23:45:39 -0400

    Try something a bit different

--- photobomb/wsgi.py
+++ photobomb/wsgi.py
@@ -8,7 +8,13 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "photobomb.settings")
+
+# Make sure current path and parent is included in sys.path for relative paths to work
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
