cb46699566700a6ea0f08b4746e430dfed0a24e9
Antonia Stevens <a@antevens.com>
2016-10-09 00:04:27 -0400

    Add debugging

--- manage.py
+++ manage.py
@@ -5,6 +5,10 @@ import sys
 # Make sure current path is included in sys.path for relative paths to work
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
+for p in sys.path:
+    print(p)
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "photobomb.settings")
 
